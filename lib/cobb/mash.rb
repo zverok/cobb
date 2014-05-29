13aa8633e589f8867caf30aa422ad22a6b781bf2
zverok <zverok.offline@gmail.com>
2014-05-29 13:49:41 +0300

    Cobb::Mash fixed to actually do what he supposed to do

--- lib/cobb/mash.rb
+++ lib/cobb/mash.rb
@@ -4,7 +4,10 @@ require 'hashie'
 module Cobb
   class Mash < Hashie::Mash
     def deep_update(other)
-       super(other.reject{|k, v| v.nil?})
+        super(other.reject{|k, v| v.nil?})
     end
+    
+    alias_method :update, :deep_update
+    alias_method :merge!, :update    
   end
 end
